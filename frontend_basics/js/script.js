b3cdb8f4797633281657979a1187d35fd25edb08
gurban-dev <dennisgurban44@gmail.com>
2026-04-06 11:23:54 -0400

    Invalid file name removed.

--- frontend_basics/js/script.js
+++ frontend_basics/js/script.js
@@ -1,7 +1,10 @@
+
 const readMoreBtn = document.getElementById("readMoreBtn");
 
 const output = document.getElementById("output");
 
+// Arrow functions are the modern standard for writing concise
+// JavaScript code (ECMAScript 6 or ES6).
 readMoreBtn.addEventListener("click", () => {
-    output.textContent = "You click the button!";
+    output.textContent = "You clicked the button!";
 });
