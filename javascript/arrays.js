b0289672b13e0745a09655a688cad8ea64e706ac
gurban-dev <dennisgurban44@gmail.com>
2026-07-06 19:40:02 +0400

    arrays.js updated.

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -1,12 +1,12 @@
 // Array composed of items with distinct data types.
-const students = ["Alice", 10, true];
+const student_info = ["Alice", 10, true];
 
 // Display the entire array with a whitespace character as
 // the separator.
-console.log(`students: ${students.join(" ")}`);
+console.log(`student_info: ${student_info.join(" ")}`);
 
 // Display the first element in the array.
-console.log(`\nstudents[0]: ${students[0]}`);
+console.log(`\nstudent_info[0]: ${student_info[0]}`);
 
 // Display the last element in the array.
 
@@ -15,11 +15,11 @@ console.log(`\nstudents[0]: ${students[0]}`);
 
 // In this context, the word "native" means that a feature
 // is built directly into the core JavaScript language.
-// console.log(`\nstudents[-1]: ${students[-1]}`);
+// console.log(`\nstudent_info[-1]: ${student_info[-1]}`);
 
 // The .at() method was introduced and it natively supports
 // negative integers.
-console.log(`\nstudents.at(-1): ${students.at(-1)}`);
+console.log(`\nstudent_info.at(-1): ${student_info.at(-1)}`);
 
 const fruits = ["orange", "madarin", "apple"];
 
@@ -28,17 +28,17 @@ fruits.push("fig");
 
 console.log(`\nfruits: ${fruits.join(" ")}`)
 
-// Remove the last item.
+// Remove the item at the end of the array.
 fruits.pop()
 
-// Insert an element at particular index.
+// Insert an element at a particular index.
 // The element at that index gets pushed to the right
 // just as the ones that follow it.
 
 // Syntax:
 // .splice(index_to_insert_at, no_of_items_to_remove, item_to_insert);
 
-// The .splice() method cannot accept keyword argument; it only
+// The .splice() method cannot accept keyword arguments; it only
 // accepts positional arguments.
 
 // Positional arguments (no parameters included):
@@ -50,6 +50,16 @@ fruits.pop()
 // The parameter names are: size, milk and is_iced
 
 fruits.splice(1, 0, "clementine");
+
+console.log(`\nfruits: ${fruits.join(" ")}`)
+
+// If an integer greater than zero is passed as an argument for
+// the no_of_items_to_remove parameter, an array containing the
+// items that were removed will be returned.
+
+const removed_fruits = fruits.splice(0, 2, "avocado");
+
+console.log(`\nremoved_fruits: ${removed_fruits.join(" ")}`)
 
 console.log(`\nfruits: ${fruits.join(" ")}`)
 
---
+++ javascript/exercises/arrays_ex.js
@@ -0,0 +1,25 @@
+/*
+Create a cart for an E-commerce website.
+
+Instructions
+
+1. Create an array named "cart" with the following items:
+   • Bread
+   • Milk
+
+2. Add "Eggs" to the end of the array.
+
+3. Display the items in the array.
+
+4. Remove the last item from the array.
+
+5. Display the array again.
+
+6. Add "Butter" to the beginning of the array.
+
+7. Display the array again.
+
+8. Remove the first item from the array.
+
+9. Display the final array.
+*/
